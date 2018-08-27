5e61a280e6866bfa1d997718d0e15338a50d20b2
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-27 17:56:48 +0300

    +

--- chapter4.js
+++ chapter4.js
@@ -47,7 +47,7 @@ x = max([1, 2, 3]);
 console.log(x);
 
 
-const getLess = (list, pivot) => {
+/* const getLess = (list, pivot) => {
     const arr = [];
     list.forEach(element => {
         if (element <= pivot) arr.push(element);
@@ -75,4 +75,50 @@ function quicksort(array) {
     }
 }
 
-console.log(quicksort([10, 5, 2, 3]));
+console.log(quicksort([10, 5, 2, 3])); */
+
+function swap(items, firstIndex, secondIndex){
+    const temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right);
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+
+// first call
+const items = [10, 5, 2, 3];
+var result = quickSort(items, 0, items.length - 1);
+console.log(result);
